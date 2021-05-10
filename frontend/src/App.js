722a44cad95182322143e2a3324b82013d442eeb
Mithu12 <mosaddekalimithu123@gmail.com>
2021-05-11 02:41:27 +0600

    Home screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import {Container} from "react-bootstrap";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 const App = () => {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Header/>
+      <main>
+          <Container>
+              <Route path='/' component={HomeScreen} exact/>
+              <Route path='/product/:id' component={ProductScreen} exact/>
+          </Container>
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
