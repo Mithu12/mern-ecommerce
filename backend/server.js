e501eed562a824f7f47b741696c4226fe30b7495
Mithu12 <mosaddekalimithu123@gmail.com>
2021-05-12 00:10:29 +0600

    product routes added

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,13 @@ dotenv.config()
 connectDB()
 const app = express()
 
+
+
 app.get('/', (req, res)=>{
     res.send('REST is active')
 })
+app.use('/api/products/', productRoutes)
 
-app.get('/api/products', (req, res)=>{
-    res.json(products)
-})
-
-app.get('/api/products/:id', (req, res)=>{
-    const product = products.find(p=>p._id === req.params.id)
-    res.json(product)
-})
 
 const PORT = process.env.PORT || 5000
 
