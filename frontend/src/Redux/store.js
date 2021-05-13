dff263b83caa53f2285296cd04ba8682c904368a
Mithu12 <mosaddekalimithu123@gmail.com>
2021-05-13 20:14:31 +0600

    cart with initial value added to store

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -2,13 +2,23 @@ import {createStore, combineReducers, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 import {composeWithDevTools} from "redux-devtools-extension";
 import { productListReducer, productDetailsReducer } from "./Product/productReducers";
+import {cartReducer} from "./Cart/cartReducer";
 
 
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart: cartReducer
 })
-const initialState = {}
+
+const cartItemsFromLocal = localStorage.getItem('cartItems') ?
+    JSON.parse(localStorage.getItem('cartItems')) : []
+
+
+const initialState = {
+    cart: {cartItems: cartItemsFromLocal}
+}
+
 const middleware = [thunk]
 
 const store = createStore(
